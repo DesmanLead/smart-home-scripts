6168a1acff8ef294648321e002dc36c9cbd0fb5f
Artem Kirienko <artem.kirienko@gmail.com>
2017-08-10 19:32:38 +0300

    Data preprocessing bug fixed

--- learn/simple_model_activities.py
+++ learn/simple_model_activities.py
@@ -3,10 +3,11 @@
 
 from preprocessing.data_loading import load
 from sklearn import svm
-# from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_curve
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+# from sklearn.metrics import roc_curve
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+# import csv
 
 
 raw_data = load('/Users/akirienko/Dropbox/Учеба/Аспирантура/SHDataActivity/Mine/*.csv')
@@ -27,7 +28,8 @@ Y = []
 
 activity_of_interest = activity_uuids[5]
 
-current_state = [None] * len(sensor_uuids)
+current_state = [0] * len(sensor_uuids)
+current_state[0] = None
 current_activity = None
 
 for record in raw_data:
@@ -35,7 +37,7 @@ for record in raw_data:
     if uuid in sensor_uuids:
         current_state[sensor_uuids.index(uuid)] = record.value()
         if None not in current_state and not record.is_from_dense_data_source():
-            X.append(current_state)
+            X.append(list(current_state))
             Y.append(1 if current_activity == activity_of_interest else -1)
     if uuid == activity_of_interest:
         if record.value() > 0.5:
@@ -43,30 +45,37 @@ for record in raw_data:
         else:
             current_activity = None
 
+activity_name = activities[activity_of_interest].description()
+print 'Evaluation results for activity: %s' % activity_name
 
-print 'Evaluation results for activity: %s' % activities[activity_of_interest].description()
+# with open('/Users/akirienko/Desktop/out.csv', 'wb') as out_file:
+#     out_csv = csv.writer(out_file)
+#
+#     for row in zip(X, Y):
+#         csv_row = row[0] + [row[1]]
+#         out_csv.writerow(csv_row)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.1,
-                                                    random_state=0)
-
+# X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.1,
+#                                                     random_state=0)
+#
 clf = svm.SVC()
-clf.fit(X_train, y_train)
-y_score = clf.decision_function(X_test)
+# clf.fit(X_train, y_train)
+# y_score = clf.decision_function(X_test)
+#
+# fpr, tpr, _ = roc_curve(y_test, y_score)
+#
+# plt.figure()
+# lw = 2
+# plt.plot(fpr, tpr, color='darkorange',
+#          lw=lw, label='ROC curve')
+# plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('Receiver operating characteristic')
+# plt.legend(loc="lower right")
+# plt.show()
 
-fpr, tpr, _ = roc_curve(y_test, y_score)
-
-plt.figure()
-lw = 2
-plt.plot(fpr, tpr, color='darkorange',
-         lw=lw, label='ROC curve')
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.show()
-
-# print 'AUC: %s' % cross_val_score(clf, X, Y, scoring='roc_auc')
-# print 'Accuracy: %s %%' % cross_val_score(clf, X, Y, scoring='accuracy')
+print 'AUC: %s' % cross_val_score(clf, X, Y, scoring='roc_auc')
+print 'Accuracy: %s %%' % cross_val_score(clf, X, Y, scoring='accuracy')
