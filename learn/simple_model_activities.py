01de191c17d4ff90a581b554ab3e325745a1b6f3
Artem Kirienko <artem.kirienko@gmail.com>
2018-02-16 14:01:42 +0300

    Evaluation for all activities implemented

--- learn/simple_model_activities.py
+++ learn/simple_model_activities.py
@@ -22,46 +22,45 @@ for record in raw_data:
     if record.is_sensor_data() and record.uuid() not in sensor_uuids:
         sensor_uuids.append(record.uuid())
 
-X = []
-Y = []
+for activity_of_interest in activity_uuids:
+    X = []
+    Y = []
 
-activity_of_interest = activity_uuids[1]
+    current_state = [0] * len(sensor_uuids)
+    current_state[0] = None
+    current_activity = None
 
-current_state = [0] * len(sensor_uuids)
-current_state[0] = None
-current_activity = None
+    ma_filters = [MovingAverage(0, size=8) for n in range(len(sensor_uuids))]
 
-ma_filters = [MovingAverage(0, size=4) for n in range(len(sensor_uuids))]
+    for record in raw_data:
+        uuid = record.uuid()
 
-for record in raw_data:
-    uuid = record.uuid()
+        if uuid in sensor_uuids:
+            index = sensor_uuids.index(uuid)
 
-    if uuid in sensor_uuids:
-        index = sensor_uuids.index(uuid)
+            # filtering
+            value = record.value()
+            if record.is_beacon_rssi() and value == 0:
+                value = -255
+            ma_filters[index].put(value)
 
-        # filtering
-        value = record.value()
-        if record.is_beacon_rssi() and value == 0:
-            value = -255
-        ma_filters[index].put(value)
+            current_state[index] = ma_filters[index].filtered_value()
 
-        current_state[index] = ma_filters[index].filtered_value()
+            # appending feature vector if needed
+            if None not in current_state and not record.is_from_dense_data_source():
+                X.append(list(current_state))
+                Y.append(1 if current_activity == activity_of_interest else -1)
 
-        # appending feature vector if needed
-        if None not in current_state and not record.is_from_dense_data_source():
-            X.append(list(current_state))
-            Y.append(1 if current_activity == activity_of_interest else -1)
+        if uuid == activity_of_interest:
+            if record.value() > 0.5:
+                current_activity = uuid
+            else:
+                current_activity = None
 
-    if uuid == activity_of_interest:
-        if record.value() > 0.5:
-            current_activity = uuid
-        else:
-            current_activity = None
+    activity_name = activities[activity_of_interest].description()
+    print 'Evaluation results for activity: %s' % activity_name
 
-activity_name = activities[activity_of_interest].description()
-print 'Evaluation results for activity: %s' % activity_name
+    clf = svm.SVC()
 
-clf = svm.SVC()
-
-print 'AUC: %s' % cross_val_score(clf, X, Y, scoring='roc_auc')
-print 'Accuracy: %s %%' % cross_val_score(clf, X, Y, scoring='accuracy')
+    print 'AUC: %s' % cross_val_score(clf, X, Y, scoring='roc_auc')
+    print 'Accuracy: %s %%' % cross_val_score(clf, X, Y, scoring='accuracy')
